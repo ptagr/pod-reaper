da4f02e8ad115c29d1e17f1afa7dbc599dca0e93
Punit Agrawal <punagrawal@ebay.com>
2017-10-17 17:25:13 -0700

    Add deps

--- main.go
+++ main.go
@@ -3,12 +3,56 @@ package main
 import (
 	"fmt"
 	"time"
+	"flag"
+	"path/filepath"
+	"os"
+	metav1 "github.com/kubernetes/apimachinery/pkg/apis/meta/v1"
+	"github.com/kubernetes/client-go/tools/clientcmd"
+	"github.com/kubernetes/client-go/kubernetes"
 )
 
 func main(){
-	for true {
-		fmt.Printf("\n%s Hello from pod reaper! Hide all the pods!", time.Now().Format(time.UnixDate))
-		time.Sleep(30 * time.Second)
+	//for true {
+	//	fmt.Printf("\n%s Hello from pod reaper! Hide all the pods!", time.Now().Format(time.UnixDate))
+	//	time.Sleep(30 * time.Second)
+	//}
+
+	fmt.Printf("\n%s Hello from pod reaper! Hide all the pods!", time.Now().Format(time.UnixDate))
+
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	fmt.Printf("\nLoading kubeconfig from %s", *kubeconfig)
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
 	}
 
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	}
+
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
